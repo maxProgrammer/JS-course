60de94da3f13190b481b26623a4c6eef80702c15
maxProgrammer <maxwneto.poo@gmail.com>
2022-05-19 08:57:56 -0300

    modelo idade

---
+++ aula06/entrada.js
@@ -0,0 +1,4 @@
+var num1 = Number(window.prompt('Digite um número: '))
+var num2 = Number(window.prompt('Digite outro número: '))
+var sum = num1 + num2
+window.alert(`A soma entre ${num1} e ${num2} é igual a ${sum}`)
---
+++ aula06/greetings.js
@@ -0,0 +1,2 @@
+var nome = window.prompt("Qual é o seu nome?")
+window.alert("Olá " + nome + "! É um grande prazer te conhecer.")
