0fb03188dbc66422132298cb2f1e234a0f608754
maxProgrammer <maxwneto.poo@gmail.com>
2022-05-19 18:21:20 -0300

    tabuada

--- aula14-exe002/script.js
+++ aula14-exe002/script.js
@@ -1,23 +1,21 @@
 function tabuada() {
     var begin = window.document.querySelector('input#txinput')
-    var res = window.document.querySelector('select#seltab')
+    var tab = window.document.querySelector('select#seltab')
     entrada = Number(begin.value)
 
     if (begin.value.length == 0) {
-        res.innerHTML = '[ERRO]. Necessário informar um número!'
+        window.alert('[ERRO]. Necessário informar um número!')
     } else {
 
-        for (var i = 1; i <= entrada; i++) {
-            res.innerHTML = `Tabuada de ${i}: `
-            for(var j = 1; j <=10; j++){
-                var result = i * j
-                res.innerHTML += `${i} * ${j} = ${result} `
+        for (var i = 1; i <= 10; i++) {
+            var item = document.createElement('option')
+            item.text = `${i} * ${entrada} = ${i * entrada} `
+            tab.appendChild(item)
 
-            }
         }
-
     }
 
 }
+
 
 
